54868f3c50236b17b1ad332086c6d02ec41da1f2
Kris Simak <k.simak@dilworth.school.nz>
2020-09-04 02:01:35 +0000

    While loop at beginning for TIE

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 
 winner = True
+tie = False
 options = ["Rock", "Paper", "Scissors"]
 #Title Screen
 
@@ -10,29 +11,33 @@ options = ["Rock", "Paper", "Scissors"]
 
 #Ask the user name
 name  = input("what's your name\n")  
-user_answer = options[int(input("\nHi " + name + " What would you like to choose?\nWrite the NUMBER OF YOUR CHOICE\n\n(0)rock\n(1)paper\n(2)scissors\n\n"))]
 
-#CPU choses at the same time as player
-      #OLD CODE cpu = random.randint(1,3)
-cpu = options[random.randint(0,2)]
+while tie == False:
+  # USER SELECTION
+  user_answer = options[int(input("\nHi " + name + " What would you like to choose?\nWrite the NUMBER OF YOUR CHOICE\n\n(0)Rock\n(1)Paper\n(2)Scissors\n\n"))]
+  #CPU SELECTION
+  cpu = options[random.randint(0,2)]
+  if user_answer == cpu:
+    tie = True
+    print ("WE HAVE A TIE")
+    print("CPU chose =", cpu,"AND YOU CHOSE =",user_answer)
+    print ("TRY AGAIN\n")
+
+
 
 # 1=Rock 2=Paper 3=Scissors
 #THIS IS THE USER CHOICE
-if user_answer == 1:
-  print ("\nYou choose ROCK")
-  #print ("The CPU chose {}" .format(cpu))
+if user_answer == options[0] :
   if cpu == options[1]:
-    print("CPU paper covers your ROCK")
+   print("CPU", cpu,"covers your",user_answer)
+   winner = False
+elif user_answer == options[1]:
+  if cpu == options[2]:
+    print("CPU", cpu,"cuts your",user_answer)
     winner = False
-elif user_answer == 2:
-  print ("You choose PAPER")
-  if cpu == 3:
-    print("CPU scissors cuts your PAPER")
-    winner = False
-elif user_answer == 3:
-  print ("You choose SCISSORS")
-  if cpu == 1:
-    print("CPU rock smashes your SCISSORS")
+elif user_answer == options[2]:
+  if cpu == options[0]:
+    print("CPU", cpu,"smashes your",user_answer)
     winner = False
 else:
   print ("choose something idiot \n ONLY CHOOSE FROM \n\n ROCK PAPER SCISSORS")
@@ -41,22 +46,20 @@ else:
 if user_answer != cpu:
   if winner == False:
     print ("YOU LOSE")
+    print("CPU chose =", cpu,"AND YOU CHOSE =",user_answer)
   else:
     print ("YOU WIN")
+    print("CPU chose =", cpu,"AND YOU CHOSE =",user_answer)
 else:
   tie = True
   print ("WE HAVE A TIE")
-
+  print("CPU chose =", cpu,"AND YOU CHOSE =",user_answer)
 
 
 #THIS IS THE  CPU WIN CHECK
 
 
 #print(name + " WINS!!!!")
-
-
-
-
 
 
 #THIS IS FOR DEBUGGING 
